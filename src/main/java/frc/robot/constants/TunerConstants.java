a776ccc0b3fd987edb84a89a021f183cf158bd50
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-01-13 23:14:34 -0800

    use torque current control for drive closed loop pathplanner

--- src/main/java/frc/robot/constants/TunerConstants.java
+++ src/main/java/frc/robot/constants/TunerConstants.java
@@ -36,7 +36,7 @@ public class TunerConstants {
       new Slot0Configs().withKP(0).withKI(0).withKD(0).withKS(0).withKV(0).withKA(0);
 
   private static final ClosedLoopOutputType steerClosedLoopOutput = ClosedLoopOutputType.Voltage;
-  private static final ClosedLoopOutputType driveClosedLoopOutput = ClosedLoopOutputType.Voltage;
+  private static final ClosedLoopOutputType driveClosedLoopOutput = ClosedLoopOutputType.TorqueCurrentFOC;
 
   // Theoretical free speed (m/s) at 12v applied output;
   public static final double kSpeedAt12VoltsMps = 5.395;
