7d5f4775ef5e1f0451567e5d8fa81852d0510c43
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-02-16 02:15:25 -0800

    change auto rotate kp to 6 for feeding

--- src/main/java/frc/robot/commands/LeftFeed.java
+++ src/main/java/frc/robot/commands/LeftFeed.java
@@ -16,7 +16,7 @@ public class LeftFeed extends Command {
   private Swerve swerve;
   private Superstructure superstructure;
 
-  private PIDController turnPID = new PIDController(7, 0, 0);
+  private PIDController turnPID = new PIDController(6, 0, 0);
 
   private double setpoint;
 
