01d1e548d58349a20de0b005f69c4e17018bfbce
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-02-22 17:26:27 -0800

    decrease shoot voltage

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -106,7 +106,7 @@ public class Constants {
     public static final double secondFeedVoltage = 1;
     public static final double thirdFeedVoltage = -0.75;
 
-    public static final double shootVoltage = 7;
+    public static final double shootVoltage = 5;
     public static final double spitVoltage = -4;
 
     public static final double shootWaitTimerSec = 0.1;
