636ae498c128c09c7d7e63f1771984108656ab6b
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-02-24 23:19:45 -0800

    reset odometry for leave path

--- src/main/java/frc/robot/autonomous/modes/Leave.java
+++ src/main/java/frc/robot/autonomous/modes/Leave.java
@@ -1,7 +1,9 @@
 package frc.robot.autonomous.modes;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 import frc.robot.subsystems.swerve.Swerve;
@@ -10,6 +12,14 @@ public class Leave extends SequentialCommandGroup {
     public Leave(Swerve swerve) {
         setName("LEAVE");
         addRequirements(swerve);
-        addCommands(AutoBuilder.followPath(Robot.Leave));
+        addCommands(new InstantCommand(
+            () -> {
+              PathPlannerPath path = Robot.Leave;
+              if (AutoBuilder.shouldFlip()) {
+                path = path.flipPath();
+              }
+              AutoBuilder.resetOdom(path.getStartingHolonomicPose().get());
+            }),
+            AutoBuilder.followPath(Robot.Leave));
     }
 }
