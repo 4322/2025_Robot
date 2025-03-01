8cb2034dc59db5586db6e9de5a18cec729706deb
Doug Kavner <dougkavner@frc4322.com>
2025-02-28 19:37:52 -0800

    Remove explicit garabge collection calls until we do more gc testing

--- src/main/java/frc/robot/commands/RightFeed.java
+++ src/main/java/frc/robot/commands/RightFeed.java
@@ -97,6 +97,5 @@ public class RightFeed extends Command {
   @Override
   public void end(boolean interrupted) {
     superstructure.requestIdle();
-    System.gc(); // run Java garbage collector before we start aligning
   }
 }
