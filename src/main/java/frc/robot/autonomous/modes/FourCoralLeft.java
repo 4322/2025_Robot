d092387ca2d2051ee696ab3e8b1d96edda1e6b3e
Dhrish Karangula <dhrishkarangula@frc4322.com>
2025-04-10 20:07:31 -0700

    fix 4 coral auto

--- src/main/java/frc/robot/autonomous/modes/FourCoralLeft.java
+++ src/main/java/frc/robot/autonomous/modes/FourCoralLeft.java
@@ -3,6 +3,7 @@ package frc.robot.autonomous.modes;
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.ScoringManager.ScoringLocation;
@@ -12,6 +13,7 @@ import frc.robot.commands.auto.AutoPreScoreCoral;
 import frc.robot.commands.auto.AutoScoreCoral;
 import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.Superstructure.Level;
+import frc.robot.subsystems.Superstructure.Superstates;
 import frc.robot.subsystems.endEffector.EndEffector;
 import frc.robot.subsystems.swerve.Swerve;
 
@@ -23,35 +25,38 @@ public class FourCoralLeft extends SequentialCommandGroup {
         new AutoPoseReset(
             swerve, Robot.FourCoralStartToKilo.getStartingHolonomicPose().get().getTranslation()),
         new InstantCommand(
-          () -> {
-            RobotContainer.operatorBoard.setScoringLevel(Level.L2);
-            RobotContainer.operatorBoard.setScoringLocation(ScoringLocation.K);
-          }),
+            () -> {
+              RobotContainer.operatorBoard.setScoringLevel(Level.L2);
+              RobotContainer.operatorBoard.setScoringLocation(ScoringLocation.K);
+            }),
         AutoBuilder.followPath(Robot.FourCoralStartToKilo),
         new AutoPreScoreCoral(swerve, superstructure, false, false),
         new AutoScoreCoral(superstructure).withTimeout(2),
-        new InstantCommand(
-          () -> {
-            RobotContainer.operatorBoard.setScoringLevel(Level.L3);
-            RobotContainer.operatorBoard.setScoringLocation(ScoringLocation.K);
-          }),
+        new WaitUntilCommand(() -> superstructure.getState() == Superstates.IDLE),
         new AutoLeftFeedCoral(swerve, superstructure, endEffector, false),
+        new InstantCommand(
+            () -> {
+              RobotContainer.operatorBoard.setScoringLevel(Level.L3);
+              RobotContainer.operatorBoard.setScoringLocation(ScoringLocation.K);
+            }),
         new AutoPreScoreCoral(swerve, superstructure, false, false),
         new AutoScoreCoral(superstructure).withTimeout(2),
-        new InstantCommand(
-          () -> {
-            RobotContainer.operatorBoard.setScoringLevel(Level.L3);
-            RobotContainer.operatorBoard.setScoringLocation(ScoringLocation.L);
-          }),
+        new WaitUntilCommand(() -> superstructure.getState() == Superstates.IDLE),
         new AutoLeftFeedCoral(swerve, superstructure, endEffector, false),
+        new InstantCommand(
+            () -> {
+              RobotContainer.operatorBoard.setScoringLevel(Level.L3);
+              RobotContainer.operatorBoard.setScoringLocation(ScoringLocation.L);
+            }),
         new AutoPreScoreCoral(swerve, superstructure, false, false),
         new AutoScoreCoral(superstructure).withTimeout(2),
+        new WaitUntilCommand(() -> superstructure.getState() == Superstates.IDLE),
+        new AutoLeftFeedCoral(swerve, superstructure, endEffector, false),
         new InstantCommand(
             () -> {
               RobotContainer.operatorBoard.setScoringLevel(Level.L2);
               RobotContainer.operatorBoard.setScoringLocation(ScoringLocation.L);
             }),
-        new AutoLeftFeedCoral(swerve, superstructure, endEffector, false),
         new AutoPreScoreCoral(swerve, superstructure, false, false),
         new AutoScoreCoral(superstructure).withTimeout(2));
   }
